07a8750add9ebd9d1a62fe812fd4e61675d167df
Alfonso Maldonado <ponmaldonadol@hotmail.com>
2023-05-29 12:44:10 -0600

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,8 @@
 import './globals.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
